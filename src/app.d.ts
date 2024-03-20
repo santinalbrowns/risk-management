e2f4577948c8c66da33eca8b050cd4094a569223
Santinal Browns <60613674+santinalbrowns@users.noreply.github.com>
2024-03-20 20:17:58 +0200

    update

--- src/app.d.ts
+++ src/app.d.ts
@@ -3,7 +3,12 @@
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			user: {
+				id: number;
+				email: string;
+			}
+		}
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -8,24 +8,24 @@ export async function handle({ event, resolve }) {
         throw redirect(302, '/');
     }
 
-    if (event.url.pathname.startsWith('/')) {
+    if (!event.url.pathname.startsWith('/login')) {
         if (!session) {
-            //throw redirect(302, '/login');
+            throw redirect(302, '/login');
         }
 
         // Fetch user details based on the session ID from the 'session' cookie
-        /*  const user = await database.user.findUnique({
+         const user = await database.user.findUnique({
              where: { id: Number.parseInt(session) },
-             select: { id: true, email: true, username: true, role: true }
-         }); */
+             select: { id: true, email: true }
+         });
 
         // If the user is not found, redirect to '/login'
-        /* if (!user) {
+        if (!user) {
             throw redirect(302, '/login');
-        } */
+        }
 
         // Set the authenticated user details in 'event.locals.user'
-        //event.locals.user = user;
+        event.locals.user = user;
     }
 
     // Resolve the incoming event and get the response
